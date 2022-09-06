e6b03a3080d1e90c9f792761bafe51ad54ab1ced
Kelly Nelson <kelnel86@gmail.com>
2022-09-06 14:02:50 -0500

    adds new route and new.ejs

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const port = 3000;
 
+
+/// ROUTES ////
+
+// INDEX //
+
+// NEW //
+app.get("/logs/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// DELETE //
+
+// UPDATE //
+
+// CREATE //
+
+// EDIT //
+
+// SHOW //
+
 /// Listener ///
 app.listen(port, () => {
     console.log("Listening on port", port);
