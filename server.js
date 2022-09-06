573f257a06a890c2aa2b69814ec9d663410bbbb0
Kelly Nelson <kelnel86@gmail.com>
2022-09-06 14:26:42 -0500

    creates model, schema, and links create route to mongoDB

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const port = 3000;
+const Log = require("./models/logs.js")
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
 
 // Middleware
 // Body parser middleware: give us access to req.body
@@ -30,7 +46,9 @@ app.post("/logs", (req, res) => {
     } else {
         req.body.shipIsBroken = false
     }
-    res.send(req.body)
+    Log.create(req.body, (error, createdLog) => {
+        res.redirect("/logs")
+    })
 })
 
 // EDIT //
